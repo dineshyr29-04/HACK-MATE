8317f13353b56ecc37e802b291e6951a0031ec3f
Anandmahadev <anandgowda82961@gmail.com>
2026-02-27 22:26:43 +0530

    refactor: optimize effect dependencies in App hook

--- src/components/PromptModal.tsx
+++ src/components/PromptModal.tsx
@@ -185,3 +185,5 @@ export function PromptModal({ isOpen, onClose, onSubmit, title, placeholder, def
 // build refinement iteration 36
 
 // build refinement iteration 37
+
+// build refinement iteration 38
