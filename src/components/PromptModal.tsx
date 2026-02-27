cd634a698a55a72fa764ca78c4e70090077d7106
Anandmahadev <anandgowda82961@gmail.com>
2026-02-27 22:26:41 +0530

    style: refine glassmorphism effect on navbar

--- src/components/PromptModal.tsx
+++ src/components/PromptModal.tsx
@@ -183,3 +183,5 @@ export function PromptModal({ isOpen, onClose, onSubmit, title, placeholder, def
 // build refinement iteration 35
 
 // build refinement iteration 36
+
+// build refinement iteration 37
