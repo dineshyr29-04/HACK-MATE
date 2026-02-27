7c21a884c52473d7d1d985d56f89656c52bb6a38
Anandmahadev <anandgowda82961@gmail.com>
2026-02-27 22:26:39 +0530

    style: update active tab indicator in StageDetail

--- src/components/PromptModal.tsx
+++ src/components/PromptModal.tsx
@@ -179,3 +179,5 @@ export function PromptModal({ isOpen, onClose, onSubmit, title, placeholder, def
 // build refinement iteration 33
 
 // build refinement iteration 34
+
+// build refinement iteration 35
